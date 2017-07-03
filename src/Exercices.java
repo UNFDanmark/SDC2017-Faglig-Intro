8b2c091802b73156a020ec91df535235516bc7ee
Steffen Strunge Mathiesen <st.st.ma@hotmail.com>
2017-07-03 17:58:02 +0200

    Added project

---
+++ src/Exercices.java
@@ -0,0 +1,134 @@
+
+public class Exercices {
+
+	/** Opgave 1
+     * Udfyld metoden så den skriver jeres navne ud på skærmen.
+     */
+	public void print() {
+	}
+
+    /**
+     * Opgave 2
+     * Opret variable for værdierne 10, 10.6, 'a', true, 10.6 og "UNF".
+	 * Note: brug ikke nogen access modifiers, såsom private eller public
+     */
+
+    /**
+     * Opgave 3
+     * Input: a, b og c som heltal
+     * Udfyld metoden så den ganger a med b, lægger c til og trækker a fra.
+     * */
+	public int expression(int a, int b, int c) {
+		return 0;
+	}
+
+    /**
+     * Opgave 4
+     * Input: vægt og højde som reelle tal
+     * Udfyld metoden så den returnerer BMI.
+	 * Hint: BMI regnes ved at tage ens vægt i kg og dividere med højde i meter to gange.
+     */
+	public double bmi(double weight, double height) {
+		return 0;
+	}
+
+	// #########################################################
+	// ##### Stop her - resten er til efter 2. forelæsning #####
+	// #########################################################
+
+    /**
+     * Opgave 5.1 - Gammel nok til at komme ind på diskotekerne i Gaden?
+     * Input: alder som heltal
+     * Udfyld metoden så den returnerer sand hvis man er gammel nok, ellers false.
+     */
+	public boolean bouncerDK(int age) {
+		return true;
+	}
+
+    /**
+     * Opgave 5.2 - Hvad hvis man er i USA?
+     * Input: alder som heltal
+     */
+	public boolean bouncerUSA(int age) {
+		return true;
+	}
+
+    /**
+     * Opgave 5.3 - Hvad hvis man er tre som går i gaden sammen og "danser"?
+	 * Kan alle så komme ind?
+     * Input: tre aldre
+     */
+	public boolean groupDancing(int age1, int age2, int age3) {
+		return true;
+	}
+
+    /**
+     * Opgave 6.1
+     * I en underlig fantasiverden kan man komme ind på diskotek uanset alder så længe man er flot.
+     * Input: En meget fiktiv persons alder og om vedkommende er flot
+     * Returnér hvorvidt vedkommende kan komme ind på diskotek i vores fantasiverden.
+     */
+	public boolean flotBouncer1(double age, boolean goodLooking) {
+		return true;
+	}
+
+    /**
+     * Opgave 6.2
+     * I en anden underlig fantasiverden kan man kun komme ind på diskotek hvis man er flot og endnu ikke er fyldt 40.
+     * Input: En meget fiktiv persons alder og om vedkommende er flot
+     * Returnér hvorvidt vedkommende kan komme ind på diskotek i vores anden fantasiverden.
+     */
+	public boolean flotBouncer2(double age, boolean goodLooking){
+		return true;
+	}
+
+    /**
+     *Opgave 6.3
+     * I en irriterende verden som kun kan eksistere i hovedet på en gal faglig hjælper kan man kun komme ind på diskotek hvis man:
+     *
+     * - endnu ikke er fyldt 50
+     * - ikke er præcis 25 år
+     * - er fyldt 18
+     * eller
+     * - er fyldt 16 hvis bare man er flot
+     *
+     * Input: En meget fiktiv persons alder og om vedkommende er flot
+     * Returnér om vedkommende kan komme ind på diskotek i den irriterende verden
+     */
+	public boolean flotBouncer3(double age, boolean goodLooking){
+		return true;
+	}
+
+    // Hvis der er mere tid...
+
+    /**
+     * Opgave 6.bonus
+     * En meget langsom bouncer har skrevet den følgende horrible kode.
+     * Prøv om du kan simplificere den ved at fjerne unødig kode.
+     * Denne opgave er af mindre relevans og testes ikke
+     */
+	public boolean flotBouncerBonus(double age, boolean goodLooking){
+		if((goodLooking == true) && age != 50){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+    /**
+     * Opgave 7
+     * Valutaomregner: 100 DKK er 15.3626 USD, 100 DKK er 13.4476541 EUR og 100 USD er 87.535014 EUR.
+     */
+	public double cashExchange(double amount, String from, String to) {
+		return 0;
+	}
+
+
+	/**
+	 * Opgave 8 - Fakultet
+	 * Udfyld metoden så den returnerer n!, brug ikke n over 10000. (HINT, brug recursion)
+	 */
+	public long factorial(long n) {
+		return 0;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,141 @@
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Opg 1
+		Exercices types = new Exercices();
+
+		String output = changeOut(types, "print");
+		System.out.println(responce(1, output.length() > 0) + " " + output);
+
+		// Opg 2
+		HashMap<Class<?>, Integer> seen = new HashMap<Class<?>, Integer>();
+		seen.put(int.class, 0); seen.put(double.class, 0); seen.put(char.class, 0); seen.put(boolean.class, 0); seen.put(String.class, 0);
+		if(types.getClass().getDeclaredFields().length > 4) {
+			for(Field f : types.getClass().getDeclaredFields()) {
+				try {
+					if((f.getType() == int.class && f.getInt(types) == 10)
+							|| (f.getType() == double.class && f.getDouble(types) == 10.6)
+							|| (f.getType() == char.class && f.getChar(types) == 'a')
+							|| (f.getType() == boolean.class && f.getBoolean(types) == true)
+							|| (f.getType() == String.class && f.get(types).equals("UNF"))) {
+						seen.replace(f.getType(), seen.get(f.getType()) + 1);
+					}
+				} catch(IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println(responce(2, !seen.containsValue(0)));
+
+
+		// Opg 3
+		System.out.println(responce(3, types.expression(13, 66, 4) == 849 && types.expression(43, 22, 5) == 908));
+
+
+		// Opg 4
+		System.out.println(responce(4, types.bmi(70, 1.8) == 70/(1.8*1.8) && types.bmi(60, 1.5) == 60/(1.5*1.5)));
+
+
+		// Opg 5.1
+		System.out.println(responce(5.1, types.bouncerDK(20) && !types.bouncerDK(16)));
+
+		// Opg 5.2
+		System.out.println(responce(5.2, !types.bouncerUSA(20) && types.bouncerUSA(40)));
+
+		// Opg 5.3
+		System.out.println(responce(5.3, types.groupDancing(40, 99, 300000) && !types.groupDancing(30, 17, 20)));
+
+
+		// Opg 6.1
+		System.out.println(responce(6.1, !types.flotBouncer1(12.3, false)
+				&& types.flotBouncer1(9.0, true)
+				&& types.flotBouncer1(19.5, false)
+				&& types.flotBouncer1(18.0, false)
+				&& !types.flotBouncer1(17.999, false)
+				&& types.flotBouncer1(60.7, true)));
+
+
+		// Opg 6.2
+		System.out.println(responce(6.2, !types.flotBouncer2(30.0, false)
+				&& !types.flotBouncer2(40.0, true)
+				&& types.flotBouncer2(2.4, true)
+				&& types.flotBouncer2(39.99999999, true)
+				&& !types.flotBouncer2(50.6, false)));
+
+		// Opg 6.3
+		System.out.println(responce(6.3, types.flotBouncer3(16, true)
+				&& !types.flotBouncer3(17, false)
+				&& !types.flotBouncer3(15.99, true)
+				&& !types.flotBouncer3(25, false)
+				&& !types.flotBouncer3(50, true)
+				&& types.flotBouncer3(49.999, false)
+				&& types.flotBouncer3(20, false)
+				&& types.flotBouncer3(17, true)));
+
+
+		// Opg7
+		System.out.println(responce(7, types.cashExchange(5000000, "DKK", "EUR") == 5000000 * 13.4476541 / 100
+				&& types.cashExchange(types.cashExchange(5000000, "DKK", "EUR"), "EUR", "DKK") == 5000000
+				&& types.cashExchange(23, "USD", "EUR") == 23 * 87.535014 / 100
+				&& types.cashExchange(types.cashExchange(23, "USD", "EUR"), "EUR", "USD") == 23
+				&& types.cashExchange(78.64, "USD", "DKK") == 78.64 * 100 / 15.3626
+				&& types.cashExchange(types.cashExchange(78.64, "USD", "DKK"), "DKK", "USD") == 78.64
+				&& types.cashExchange(0.33425324, "DKK", "DKK") == 0.33425324
+				&& types.cashExchange(36436, "EUR", "EUR") == 36436
+				&& types.cashExchange(436.33425324, "USD", "USD") == 436.33425324));
+
+
+		// Opg8
+		System.out.println(responce(8, types.factorial(20) == 2432902008176640000L
+				&& types.factorial(10) == 3628800));
+	}
+
+
+
+	public static String responce(int opg, boolean bool) {
+		if(bool) {
+			return "Exercise " + opg + " is correct!";
+		} else {
+			return "Exercise " + opg + " is wrong.";
+		}
+	}
+
+	public static String responce(double opg, boolean bool) {
+		if(bool) {
+			return "Exercise " + opg + " is correct!";
+		} else {
+			return "Exercise " + opg + " is wrong.";
+		}
+	}
+
+
+	public static String changeOut(Object instance, String method) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream stdout = System.out;
+		PrintStream ps = new PrintStream(baos);
+		System.setOut(ps);
+		try {
+			instance.getClass().getMethod(method).invoke(instance);
+		} catch(NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch(InvocationTargetException e) {
+			e.printStackTrace();
+		} catch(IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		System.setOut(stdout);
+		String output = new String(baos.toByteArray(), StandardCharsets.UTF_8).replace("\n", "");
+		return output;
+	}
+
+}
